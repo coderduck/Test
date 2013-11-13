b630eafe69839ae402793ef817ec70e343fd1f0e
Mark Tran <marktran@marks-air.home>
2013-11-12 22:33:14 -0500

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Mark Tran on 11/12/13.
+//  Copyright (c) 2013 Mark Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Mark Tran on 11/12/13.
+//  Copyright (c) 2013 Mark Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
