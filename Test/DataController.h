bf475350e4df093d5df9f87f3823db3519a68f15
coderduck <dw.marktran@gmail.com>
2013-11-13 02:36:16 -0500

    added Model class

---
+++ Test/DataController.h
@@ -0,0 +1,13 @@
+//
+//  DataController.h
+//  Test
+//
+//  Created by Mark Tran on 11/13/13.
+//  Copyright (c) 2013 Mark Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataController : NSObject
+
+@end
---
+++ Test/DataController.m
@@ -0,0 +1,13 @@
+//
+//  DataController.m
+//  Test
+//
+//  Created by Mark Tran on 11/13/13.
+//  Copyright (c) 2013 Mark Tran. All rights reserved.
+//
+
+#import "DataController.h"
+
+@implementation DataController
+
+@end
