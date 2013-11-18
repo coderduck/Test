34ce6ebcd8b4e52ad4958ac17578723acb0b4ee4
Edward Jo <Ed@edwards-mbp.home>
2013-11-17 20:36:42 -0500

    added another page, 1st main page, 2nd, user info, 3 dog info, in future will have to link user with doginfo pages

---
+++ Test/DogServicesViewController.m
@@ -0,0 +1,38 @@
+//
+//  DogServicesViewController.m
+//  Test
+//
+//  Created by Edward Jo on 11/17/13.
+//  Copyright (c) 2013 Mark Tran. All rights reserved.
+//
+
+#import "DogServicesViewController.h"
+
+@interface DogServicesViewController ()
+
+@end
+
+@implementation DogServicesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
